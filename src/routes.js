9123734e2783263d5f677a34e2253ced4f33a875
Milena <milenaalmeidadev@hotmail.com>
2020-12-04 16:25:45 -0400

    Página de Busca

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,15 @@ const multer = require('./app/middlewares/multer');
 
 const ProductController = require('./app/controllers/ProductController');
 const HomeController = require('./app/controllers/HomeController');
+const SearchController = require('./app/controllers/SearchController');
 
+//Home
 routes.get('/', HomeController.index);
 
+//Search
+routes.get('/products/search', SearchController.index);
+
+//Products
 routes.get('/products/create', ProductController.create);
 routes.get('/products/:id', ProductController.show);
 routes.get('/products/:id/edit', ProductController.edit);
