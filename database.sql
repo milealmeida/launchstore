73f196f02437a23dc66453019168d6840d500b39
Milena <milenaalmeidadev@hotmail.com>
2020-12-05 23:19:32 -0400

    Apresentaçao do módulo e SQL da tabela de usuários

--- database.sql
+++ database.sql
@@ -1,4 +1,9 @@
-CREATE TABLE "products" (
+DROP DATABASE IF EXISTS launchstoredb;
+CREATE DATABASE launchstoredb;
+
+CREATE TABLE "categories" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULLCREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" int NOT NULL,
   "user_id" int,
@@ -11,10 +16,6 @@ CREATE TABLE "products" (
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
 );
-
-CREATE TABLE "categories" (
-  "id" SERIAL PRIMARY KEY,
-  "name" text NOT NULL
 );
 
 CREATE TABLE "files" (
@@ -27,3 +28,39 @@ CREATE TABLE "files" (
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpf_cnpj" int UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+-- foreing keys
+ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+-- create procedure
+CREATE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated.at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgql;
+
+--auto updated_at products
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+--auto updated_at users
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
