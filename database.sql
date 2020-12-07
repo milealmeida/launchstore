042bc7a6e2fb5c41be776f3a0be80521f8be4586
Milena <milenaalmeidadev@hotmail.com>
2020-12-07 17:30:40 -0400

    start login

--- database.sql
+++ database.sql
@@ -1,9 +1,7 @@
 DROP DATABASE IF EXISTS launchstoredb;
 CREATE DATABASE launchstoredb;
 
-CREATE TABLE "categories" (
-  "id" SERIAL PRIMARY KEY,
-  "name" text NOT NULLCREATE TABLE "products" (
+CREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" int NOT NULL,
   "user_id" int,
@@ -16,7 +14,15 @@ CREATE TABLE "categories" (
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
 );
+
+CREATE TABLE "categories" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL
 );
+
+INSERT INTO categories(name) VALUES ('comida');
+INSERT INTO categories(name) VALUES ('eletrônicos');
+INSERT INTO categories(name) VALUES ('automóveis');
 
 CREATE TABLE "files" (
   "id" SERIAL PRIMARY KEY,
@@ -26,7 +32,6 @@ CREATE TABLE "files" (
 );
 
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
-
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 
 CREATE TABLE "users" (
@@ -36,30 +41,30 @@ CREATE TABLE "users" (
   "password" text NOT NULL,
   "cpf_cnpj" int UNIQUE NOT NULL,
   "cep" text,
-  "address" text,
+  "address"text,
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
 );
 
--- foreing keys
+--foreign key
 ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
--- create procedure
-CREATE FUNCTION trigger_set_timestamp()
+--create procedure
+CREATE FUNCTION trigger_set_timestamp() 
 RETURNS TRIGGER AS $$
 BEGIN
-  NEW.updated.at = NOW();
+  NEW.updated_at = NOW();
   RETURN NEW;
 END;
-$$ LANGUAGE plpgql;
+$$ LANGUAGE plpgsql;
 
---auto updated_at products
+-- auto updated_at products
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON products
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
---auto updated_at users
+-- auto updated_at users
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON users
 FOR EACH ROW
