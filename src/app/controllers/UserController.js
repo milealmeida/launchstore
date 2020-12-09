1c1580069a2f7dcd5d06aff4fd7902f003efae1a
Milena <milenaalmeidadev@hotmail.com>
2020-12-09 10:54:09 -0400

    Ajustando validação do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,18 +1,34 @@
 const User = require('../models/User');
+const { formatCpfCnpj, formatCep } = require('../../lib/utils');
 
 module.exports = {
     registerForm(req, res){
         return res.render('user/register');
     },
 
-    show(req, res){
-        return res.send('Ok, cadastrado');
+    async show(req, res){
+        const { user } = req;
+
+        user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj);
+        user.cep = formatCep(user.cep);
+
+        return res.render('user/index', { user });
     },
 
     async post(req, res){
 
         const userId = await User.create(req.body);
 
+        req.session.userId = userId;
+
         return res.redirect('/users');
+    },
+
+    async update(req, res){
+        //all fields
+
+        //has password
+
+        //password match
     }
 }
