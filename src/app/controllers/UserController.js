3ddd7026284f18912b5efae998c47efc5c06a6a9
Milena <milenaalmeidadev@hotmail.com>
2020-12-09 08:57:08 -0400

    Segurança

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,31 +5,14 @@ module.exports = {
         return res.render('user/register');
     },
 
+    show(req, res){
+        return res.send('Ok, cadastrado');
+    },
+
     async post(req, res){
-        //check if has all fields
-        const keys = Object.keys(req.body);
 
-        for(key of keys){
-            if(req.body[key] == ""){
-                return res.send('Please fill all fields!');
-            }
-        }
+        const userId = await User.create(req.body);
 
-        //check if user exists [email, cpf_cnpj]
-        const { email, cpf_cnpj, password, passwordRepeat } = req.body;
-        
-        cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
-        
-        const user = await User.findOne({
-            where: {email},
-            or: {cpf_cnpj}
-        });
-
-        if(user) return res.send('User exists');
-
-        //check if password match
-        if(password != passwordRepeat) return res.send('Password does not match!');
-
-        return res.send('Passed!');
+        return res.redirect('/users');
     }
 }
