e7b7c141f836a1dcbfeb393c949183036b1714b9
Milena <milenaalmeidadev@hotmail.com>
2020-12-11 09:30:41 -0400

    Enviando email com Nodemailer

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,8 @@
+const User = require('../models/User');
+
+const crypto = require('crypto');
+const mailer = require('../../lib/mailer');
+
 module.exports = {
     loginForm(req, res){
         return res.render('session/login');
@@ -12,5 +17,53 @@ module.exports = {
     logout(req, res){
         req.session.destroy();
         return res.redirect('/');
+    },
+
+    forgotForm(req, res){
+        return res.render('session/forgot-password');
+    },
+
+    async forgot(req, res){
+        const user = req.user;
+
+        try{
+            //um token para esse usuário
+            const token = crypto.randomBytes(20).toString('hex');
+
+            //criar uma expiração
+            let now = new Date();
+            now = now.setHours(now.getHours() + 1);
+
+            await User.update(user.id, {
+                reset_token: token,
+                reset_token_expires: now
+            });
+
+            //enviar um email com um link de recuperação de senha
+            await mailer.sendMail({
+                to: user.email,
+                from: 'no-reply@launchstore.com.br',
+                subject: 'Recuperação de senha',
+                html: `<h2>Esqueceu a senha?</h2>
+                <p>Não se preocupe, clique no link abaixo para recuperar sua senha</p>
+                <p>
+                    <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
+                        RECUPERAR SENHA
+                    </a>
+                </p>
+                `,
+            });
+
+            //avisar o usuário que eviamos o email
+            return res.render('session/forgot-password', {
+                success: 'Enviamos um link no seu email para a recuperação da senha!'
+            });
+
+        }catch(err){
+            console.log(err);
+            return res.render('session/forgot-password', {
+                error: 'Eita, parece que algo aconteceu, tente novamente!'
+            });
+        }
     }
 }
--- src/app/validators/session.js
+++ src/app/validators/session.js
@@ -23,6 +23,28 @@ async function login(req, res, next){
     next();
 }
 
+async function forgot(req, res, next){
+    const { email } = req.body;
+
+    try{
+
+        let user = await User.findOne({ where: { email } });
+
+        if(!user) return res.render('session/forgot-password', {
+            user: req.body,
+            error: 'Ops, email não cadastrado'
+        });
+
+        req.user = user;
+
+        next();
+
+    }catch(err){
+        console.error(err);
+    }
+}
+
 module.exports = {
-    login
+    login,
+    forgot
 }
