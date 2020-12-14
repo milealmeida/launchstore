de0de4bbf259aa2f171cb64a69ea359c3ba15d0a
Milena <milenaalmeidadev@hotmail.com>
2020-12-14 17:07:12 -0400

    Padrão MVC

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -3,28 +3,12 @@ const { hash } = require('bcryptjs');
 const fs = require('fs');
 
 const Product = require('../models/Product');
+const Base = require('./Base');
+
+Base.init({ table:users });
 
 module.exports = {
-    
-    async findOne(filters){
-        let query = 'SELECT * FROM users';
-
-        Object.keys(filters).map(key => {
-            // WHERE | OR | AND
-            query = `${query}
-            ${key}
-            `
-            Object.keys(filters[key]).map(field => {
-                query = `${query} ${field} = '${filters[key][field]}'`
-            });
-
-        });
-
-        const results = await db.query(query);
-
-        return results.rows[0];
-    },
-
+    ...Base,
     async create(data){
         try{
             const query = `
